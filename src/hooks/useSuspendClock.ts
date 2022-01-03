397a95949ab33c8f305d8710c395b1690175c059
nishiurahiroki@gmail.com <nishiurahiroki@gmail.com>
2022-01-03 03:11:09 +0000

    Create interval custom hook.

---
+++ src/hooks/useSuspendClock.ts
@@ -0,0 +1,48 @@
+import {useRef, useState, useEffect} from 'react'
+
+import DateDiff from 'date-diff'
+
+export function useSuspendClock({
+  interval, now, suspended
+}) {
+  const nowSeconds = useRef(new Date(now).getTime())
+
+  const [day, setDay] = useState(0)
+  const [hour, setHour] = useState(0)
+  const [minute, setMinute] = useState(0)
+
+  useEffect(() => {
+    let timer = setTimeout(function main() {
+      const diff = new DateDiff(new Date(nowSeconds.current), suspended)
+
+      const day = diff.seconds() / 86400
+      const surplusDay = diff.seconds() % 86400
+      const hour = surplusDay / 3600
+      const surplusHour = surplusDay % 3600
+      const minute = surplusHour / 60
+  
+      setMinute(Math.floor(minute))
+      setHour(Math.floor(hour))
+      setDay(Math.floor(day - 1))
+
+      nowSeconds.current = nowSeconds.current + interval
+      timer = setTimeout(main, interval)
+    }, interval)
+
+    // スマホのロックからの復帰や、タブ遷移した際に時間が止まる件の対策
+    if (typeof window === 'object') { 
+      document.addEventListener('visibilitychange', () => {
+        (async () => {
+          if (document.visibilityState === 'visible') {
+            const response = await fetch('/api/now')
+            const json = await response.json()
+            nowSeconds.current = new Date(json.datetime).getTime()
+          }
+        })()
+      })
+    }
+    return () => clearTimeout(timer)
+  }, [])
+
+  return { day, hour, minute }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
-import {useEffect, useState, useRef} from 'react'
-
-import DateDiff from 'date-diff'
+import { useSuspendClock } from '../hooks/useSuspendClock'
 
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
@@ -11,45 +9,11 @@ const INTERVAL = 1000
 const SUSPENDED_DAY = new Date(2018, 11, 26)
 
 export default function Home({ now }) {
-  const nowSeconds = useRef(new Date(now).getTime())
-
-  const [day, setDay] = useState('')
-  const [hour, setHour] = useState('')
-  const [minute, setMinute] = useState('')
-
-  useEffect(() => {
-    let timer = setTimeout(function main() {
-      const diff = new DateDiff(new Date(nowSeconds.current), SUSPENDED_DAY)
-  
-      const day = diff.seconds() / 86400
-      const surplusDay = diff.seconds() % 86400
-      const hour = surplusDay / 3600
-      const surplusHour = surplusDay % 3600
-      const minute = surplusHour / 60
-  
-      setMinute(Math.floor(minute))
-      setHour(Math.floor(hour))
-      setDay(Math.floor(day - 1))
-
-      nowSeconds.current = nowSeconds.current + INTERVAL
-      timer = setTimeout(main, INTERVAL)
-    }, INTERVAL)
-
-    // スマホのロックからの復帰や、タブ遷移した際に時間が止まる件の対策
-    if (typeof window === 'object') { 
-      document.addEventListener('visibilitychange', () => {
-        (async () => {
-          if (document.visibilityState === 'visible') {
-            // ↓TODO API直呼びでDateUtilsと処理が散らばってしまっている
-            const response = await fetch('/api/now')
-            const json = await response.json()
-            nowSeconds.current = new Date(json.datetime).getTime()
-          }
-        })()
-      })
-    }
-    return () => clearTimeout(timer)
-  }, [])
+  const { day, hour, minute } = useSuspendClock({
+    interval : INTERVAL,
+    suspended : SUSPENDED_DAY,
+    now
+  })
 
   return (
     <div className={styles.container}>
