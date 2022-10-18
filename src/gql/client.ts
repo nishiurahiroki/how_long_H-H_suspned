395159aec329cc2249223875ba57a0698016e2eb
nishiurahiroki@gmail.com <nishiurahiroki@gmail.com>
2022-10-18 15:56:38 +0000

    Backend host

--- src/gql/client.ts
+++ src/gql/client.ts
@@ -1,8 +1,11 @@
 
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
+    uri: process.env.BACKEND_HOST,
     cache: new InMemoryCache(),
 });
 
