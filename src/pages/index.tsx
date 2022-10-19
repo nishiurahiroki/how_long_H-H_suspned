f9af64de40f0dd92b8f12c0d344bb21c482a131b
nishiurahiroki@gmail.com <nishiurahiroki@gmail.com>
2022-10-19 14:00:22 +0000

    Fix indent.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,7 @@ export async function getServerSideProps({res}) {
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=10, stale-while-revalidate=59'
-)
+  )
 
   const { data } = await apolloClient.query({
     query: gql`
